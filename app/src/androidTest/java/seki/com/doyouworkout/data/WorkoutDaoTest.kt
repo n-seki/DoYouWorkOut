12ec1b5d2bf2e8fcb2d590410ff2e640922ec0dc
n-seki <developer.n.seki@gmail.com>
2018-06-16 18:34:59 +0900

    create WorkoutDaoTest class

---
+++ app/src/androidTest/java/seki/com/doyouworkout/data/WorkoutDaoTest.kt
@@ -0,0 +1,41 @@
+package seki.com.doyouworkout.data
+
+import android.arch.persistence.room.Room
+import android.support.test.InstrumentationRegistry
+import android.support.test.filters.SmallTest
+import android.support.test.runner.AndroidJUnit4
+import org.hamcrest.Matchers.`is`
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
+@RunWith(AndroidJUnit4::class) @SmallTest
+class WorkoutDaoTest {
+
+    lateinit var db: AppDataBase
+    lateinit var dao: WorkoutDao
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getTargetContext()
+        db = Room.inMemoryDatabaseBuilder(context, AppDataBase::class.java).build()
+        dao = db.workoutDao()
+    }
+
+    @After
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    fun `workoutがインサートできることのテスト`() {
+        val workout = Workout(Date(), 1, 1)
+        dao.intert(workout)
+
+        val actual: List<Workout> = dao.load()
+        assertThat(actual[0].trainingId, `is`(workout.trainingId))
+    }
+}
