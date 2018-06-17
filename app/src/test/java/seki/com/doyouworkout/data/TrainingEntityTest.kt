5ef2c1a69c349908f6bac9163a2fd8fbf13c6906
n-seki <developer.n.seki@gmail.com>
2018-06-17 12:22:00 +0900

    create training entity test class

---
+++ app/src/test/java/seki/com/doyouworkout/data/TrainingEntityTest.kt
@@ -0,0 +1,44 @@
+package seki.com.doyouworkout.data
+
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class TrainingEntityTest {
+
+    @Test
+    fun `used = 1の場合にisUsedがtrueとなること`() {
+        val training = TrainingEntity(id = 1, trainingNameId = 1, used = 1)
+        assertThat(training.isUsed, `is`(true))
+    }
+
+    @Test
+    fun `used = 0の場合にisUsedがfalseとなること`() {
+        val training = TrainingEntity(id = 1, trainingNameId = 1, used = 0)
+        assertThat(training.isUsed, `is`(false))
+    }
+
+    @Test
+    fun `custom = 1の場合にisCustomがtrueとなること`() {
+        val training = TrainingEntity(id = 1, trainingNameId = 1, used = 1, custom = 1)
+        assertThat(training.isCustom, `is`(true))
+    }
+
+    @Test
+    fun `custom = 0の場合にisCustomがfalseとなること`() {
+        val training = TrainingEntity(id = 1, trainingNameId = 1, used = 1, custom = 0)
+        assertThat(training.isCustom, `is`(false))
+    }
+
+    @Test
+    fun `delete = 1の場合にisDeletedがtrueとなること`() {
+        val training = TrainingEntity(id = 1, trainingNameId = 1, used = 1, delete = 1)
+        assertThat(training.isDeleted, `is`(true))
+    }
+
+    @Test
+    fun `delete = 0の場合にisDeletedがfalseとなること`() {
+        val training = TrainingEntity(id = 1, trainingNameId = 1, used = 1, delete = 0)
+        assertThat(training.isDeleted, `is`(false))
+    }
+}
