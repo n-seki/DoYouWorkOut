8b2fe26280c0a2e634b8f15e0aba7a80df4c93dc
n-seki <developer.n.seki@gmail.com>
2018-06-17 12:12:36 +0900

    add type converters

--- app/src/main/java/seki/com/doyouworkout/data/AppDataBase.kt
+++ app/src/main/java/seki/com/doyouworkout/data/AppDataBase.kt
@@ -2,8 +2,10 @@ package seki.com.doyouworkout.data
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.TypeConverters
 
 @Database(entities = [TrainingEntity::class, WorkoutEntity::class], version = 1)
+@TypeConverters(value = [TypeConverters::class])
 abstract class AppDataBase: RoomDatabase() {
     abstract fun trainingDao(): TrainingDao
     abstract fun workoutDao(): WorkoutDao
