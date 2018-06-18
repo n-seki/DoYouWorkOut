5af2ae0c75e6f33cf4b0eeb437822fc568bba752
n-seki <developer.n.seki@gmail.com>
2018-06-18 23:28:20 +0900

    create entity mapper

---
+++ app/src/main/java/seki/com/doyouworkout/data/db/mapper/WorkoutMapper.kt
@@ -0,0 +1,31 @@
+package seki.com.doyouworkout.data.db.mapper
+
+import android.content.Context
+import seki.com.doyouworkout.data.db.TrainingEntity
+import seki.com.doyouworkout.data.db.WorkoutEntity
+import seki.com.doyouworkout.ui.mainlist.Training
+import seki.com.doyouworkout.ui.mainlist.Workout
+import javax.inject.Inject
+
+class WorkoutMapper @Inject constructor(private val context: Context) {
+
+    fun toWorkout(workoutEntities: List<WorkoutEntity>, trainingEntities: List<TrainingEntity>): Workout {
+        val trainingEntityMap: Map<Int, TrainingEntity> = trainingEntities.associateBy { it.id }
+
+        val trainingList: MutableList<Training> = mutableListOf()
+
+        for (workoutEntity in workoutEntities) {
+            val trainingEntity: TrainingEntity =
+                    trainingEntityMap[workoutEntity.trainingId] ?: continue
+
+            val trainingName: String =
+                    if (trainingEntity.isCustom) trainingEntity.customName
+                    else context.getString(trainingEntity.trainingNameId)
+
+            val training = Training(trainingName, workoutEntity.count)
+            trainingList += training
+        }
+
+        return Workout(workoutEntities[0].date, trainingList)
+    }
+}
