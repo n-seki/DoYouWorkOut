c76dcbc39431f8f0f2cba765951c47dcf6a510e6
n-seki <developer.n.seki@gmail.com>
2018-07-19 22:21:41 +0900

    add homeAsUp button to new screen

--- app/src/main/java/seki/com/doyouworkout/ui/newWorkout/NewWorkoutActivity.kt
+++ app/src/main/java/seki/com/doyouworkout/ui/newWorkout/NewWorkoutActivity.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main_list.*
 import seki.com.doyouworkout.App
 import seki.com.doyouworkout.R
 import seki.com.doyouworkout.ui.WorkoutViewModelFactory
@@ -25,7 +27,23 @@ class NewWorkoutActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_workout)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
 
         (application as App).appComponent.inject(this)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item ?: return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
