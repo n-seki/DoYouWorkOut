4a47b48ce548c953a3e34cb4f2e02d7f672fbe68
n-seki <developer.n.seki@gmail.com>
2018-08-19 18:07:22 +0900

    make list reverse

--- app/src/main/java/seki/com/doyouworkout/ui/Workout.kt
+++ app/src/main/java/seki/com/doyouworkout/ui/Workout.kt
@@ -1,10 +1,37 @@
 package seki.com.doyouworkout.ui
 
-import java.io.Serializable
+import android.os.Parcel
+import android.os.Parcelable
 
 data class Workout(
         val id: Int,
         val name: String,
         var count: Int,
         val isUsed: Boolean = true
-): Serializable
+): Parcelable {
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(name)
+        parcel.writeInt(count)
+        parcel.writeByte(if (isUsed) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Workout> {
+        override fun createFromParcel(parcel: Parcel): Workout {
+            return Workout(
+                    parcel.readInt(),
+                    parcel.readString(),
+                    parcel.readInt(),
+                    parcel.readByte().toInt() != 0)
+        }
+
+        override fun newArray(size: Int): Array<Workout?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
