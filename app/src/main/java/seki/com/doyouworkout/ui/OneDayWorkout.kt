b0640f5100362ee768988a8e9620746799b1ea5d
n-seki <developer.n.seki@gmail.com>
2018-08-23 22:17:22 +0900

    update mainlist ui!

--- app/src/main/java/seki/com/doyouworkout/ui/OneDayWorkout.kt
+++ app/src/main/java/seki/com/doyouworkout/ui/OneDayWorkout.kt
@@ -1,8 +1,16 @@
 package seki.com.doyouworkout.ui
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 data class OneDayWorkout(
         val trainingDate: Date,
         val workout: List<Workout>
-)
+) {
+
+    companion object {
+        val format = SimpleDateFormat("yyyy/MM/dd", Locale.US)
+    }
+
+    val dateText = format.format(trainingDate)
+}
