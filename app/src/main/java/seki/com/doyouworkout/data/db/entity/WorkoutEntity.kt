8e690591e8499d267b12449c7e6647f43b5f8075
n-seki <developer.n.seki@gmail.com>
2018-11-16 23:22:57 +0900

    Add foreign key

--- app/src/main/java/seki/com/doyouworkout/data/db/entity/WorkoutEntity.kt
+++ app/src/main/java/seki/com/doyouworkout/data/db/entity/WorkoutEntity.kt
@@ -2,9 +2,18 @@ package seki.com.doyouworkout.data.db.entity
 
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.Index
 import java.util.*
 
-@Entity(tableName = "workout", primaryKeys = ["date", "training_id"])
+@Entity(tableName = "workout",
+        primaryKeys = ["date", "training_id"],
+        foreignKeys =
+        [ForeignKey(entity = TrainingEntity::class,
+                parentColumns = arrayOf("id"),
+                childColumns = arrayOf("training_id"))
+        ],
+        indices = [(Index("training_id"))])
 data class WorkoutEntity(
         val date: Date,
         @ColumnInfo(name = "training_id") val trainingId: Int,
