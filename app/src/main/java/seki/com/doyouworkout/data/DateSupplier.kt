08bb2d5ca05d8d33166ef3fd29eab52a63db4120
n-seki <developer.n.seki@gmail.com>
2018-12-17 12:19:01 +0900

    Create Date Supplier

---
+++ app/src/main/java/seki/com/doyouworkout/data/DateSupplier.kt
@@ -0,0 +1,7 @@
+package seki.com.doyouworkout.data
+
+import java.util.*
+
+interface DateSupplier {
+    fun getToday(): Date
+}
--- app/src/main/java/seki/com/doyouworkout/data/ResourceSupplierImp.kt
+++ app/src/main/java/seki/com/doyouworkout/data/ResourceSupplierImp.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import android.support.annotation.StringRes
 import javax.inject.Inject
 
-class ResourceSupplierImp @Inject constructor(private val context: Context): ResourceSupplier {
-    override fun getString(@StringRes id: Int) = context.getString(id) ?: ""
+class ResourceSupplierImp @Inject constructor(
+        private val context: Context
+) : ResourceSupplier {
+    override fun getString(@StringRes id: Int): String {
+        return context.getString(id) ?: ""
+    }
 }
